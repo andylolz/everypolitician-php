16765a6fd3dcc189c8fd0af55106d19707598550
Andy Lulham <a.lulham@gmail.com>
2016-12-13 16:24:45 +0000

    Ensure the URL request we’re mocking is correct

--- tests/FakeResponseTrait.php
+++ tests/FakeResponseTrait.php
@@ -14,10 +14,13 @@ trait FakeResponseTrait
 
     private function mockHttpCall()
     {
+        $url = 'https://raw.githubusercontent.com/everypolitician/'
+            .'everypolitician-data/master/countries.json';
         $body = file_get_contents(__DIR__.'/data/example-countries.json');
         $response = new GuzzleHttp\Psr7\Response(200, ['Content-Type' => 'application/json'], $body);
         Mockery::mock('overload:GuzzleHttp\Client')
             ->shouldReceive('get')
+            ->with($url)
             ->once()
             ->andReturn($response);
     }
