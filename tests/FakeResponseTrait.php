f7a40592074d20855c805ec87d2935670649e211
Andy Lulham <a.lulham@gmail.com>
2016-12-13 16:24:15 +0000

    No need to return anything here

--- tests/FakeResponseTrait.php
+++ tests/FakeResponseTrait.php
@@ -16,10 +16,9 @@ trait FakeResponseTrait
     {
         $body = file_get_contents(__DIR__.'/data/example-countries.json');
         $response = new GuzzleHttp\Psr7\Response(200, ['Content-Type' => 'application/json'], $body);
-        return Mockery::mock('overload:\GuzzleHttp\Client')
+        Mockery::mock('overload:GuzzleHttp\Client')
             ->shouldReceive('get')
             ->once()
-            ->andReturn($response)
-            ->getMock();
+            ->andReturn($response);
     }
 }
